c6b80f5f5dafd82494b517c1b6d9463714793d1c
oribeee <68099997+oribeee@users.noreply.github.com>
2024-01-21 15:56:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("meButton").addEventListener("click", function() {
+    alert("Me button clicked");
+});
+
+document.getElementById("otherButton").addEventListener("click", function() {
+    alert("Other button clicked");
+});
